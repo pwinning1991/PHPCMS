711d58a8253f8819b25ddebeb77f3f7c48467a85
pwinning1991 <ftcpjw@yahoo.com>
2018-09-14 06:45:29 -0400

    added some features to the admin page

---
+++ admin/categories.php
@@ -0,0 +1,36 @@
+<?php include "includes/header.php" ?>
+
+    <div id="wrapper">
+
+<?php include "includes/navaigation.php" ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to Admin
+                            <small>Author</small>
+                        </h1>
+
+                        <div class="col-xs-6">
+                          <form c action="" method="post">
+
+                          </form>
+
+                        </div>
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+  <?php include "includes/footer.php" ?>
